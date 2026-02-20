dc4a0fd2a1f73481e5b02e42296325d980726066
Luka <luka.akhalkatsi@makingscience.com>
2026-02-20 17:31:52 +0400

    feat: add main

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from client.storage_client import GCSClient
+from utils.settings import settings
+from loader.asteroids_loader import BigQuery
+from utils.logger import logger
+import pandas as pd
+import io
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(request):
+        request_json = request.get_json(silent=True)
+        file_path = request_json.get("file_path")
 
+        if not file_path:
+            return {"error": "file_path is required"}, 400
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        try:
+            gcs = GCSClient()
+            csv_content = gcs.download_csv_as_text(file_path)
 
+            if not csv_content:
+                return {"error": f"CSV file at {file_path} is empty or missing"}, 400
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+            csv_buffer = io.StringIO(csv_content)
+            df = pd.read_csv(csv_buffer, dtype=str)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            loader = BigQuery()
+            logger.info("Start loading data to bigquery")
+            loader.load_data(df, settings.PROJECT_ID, settings.DATASET_ID, settings.TABLE_ID)
+            logger.info("Loaded successfully")
+
+            return {
+                "message": "Load complete",
+                "rows_loaded": len(df),
+                "file_path": file_path
+            }
+
+        except Exception as e:
+            return {"error": str(e)}, 500
